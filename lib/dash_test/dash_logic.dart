c2dfb6cb4860ec87e5800e33fe0c8a7ddbb5dfeb
sb-dor <avaz.shams.2002@gmail.com>
2024-09-25 14:03:46 +0500

    main ui for dashed

--- lib/dash_test/dash_logic.dart
+++ lib/dash_test/dash_logic.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 enum BirdType { constant, random }
@@ -20,5 +21,63 @@ class BirdLogic {
       case BirdType.random:
         return random.nextInt(1000);
     }
+  }
+}
+
+class BirdItem {
+  final BirdType birdType;
+  final int price;
+
+  BirdItem(this.birdType, this.price);
+}
+
+class AppState {
+  static final initialState = AppState(
+    balance: 0,
+    birds: [Bird(BirdType.constant)],
+    birdsItem: [
+      BirdItem(BirdType.constant, 25),
+      BirdItem(BirdType.random, 100),
+    ],
+  );
+
+  final int balance;
+  final List<Bird> birds;
+  final List<BirdItem> birdsItem;
+
+  AppState({
+    this.balance = 0,
+    this.birds = const [],
+    this.birdsItem = const [],
+  });
+
+  AppState copyWith({
+    int? balance,
+    List<Bird>? birds,
+    List<BirdItem>? birdsItem,
+  }) {
+    return AppState(
+      balance: balance ?? this.balance,
+      birds: birds ?? this.birds,
+      birdsItem: birdsItem ?? this.birdsItem,
+    );
+  }
+}
+
+class Store {
+  final AppState appState;
+  final _controller = StreamController<AppState>.broadcast();
+  final BirdLogic birdLogic;
+
+  Stream<AppState> get getChanges => _controller.stream;
+
+  Store(this.appState, this.birdLogic);
+
+  void buyBird(BirdItem bird) {
+    _controller.add(appState);
+  }
+
+  void earn(Bird bird) {
+    _controller.add(appState);
   }
 }
--- lib/dash_test/dash_page.dart
+++ lib/dash_test/dash_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tests/dash_test/dash_logic.dart';
 
 abstract class DashAssets {
   static const dashBlueImage = "assets/dashes/dash_blue.png";
@@ -16,6 +16,82 @@ class DashPage extends StatelessWidget {
         body: Center(
           child: Text("Dash app"),
         ),
+      ),
+    );
+  }
+}
+
+class BirdView extends StatelessWidget {
+  static const size = 60.0;
+  final BirdType birdType;
+  final VoidCallback? onTap;
+
+  const BirdView({
+    super.key,
+    required this.birdType,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: InkResponse(
+        onTap: onTap,
+        child: Image.asset(_asset(birdType)),
+      ),
+    );
+  }
+
+  String _asset(BirdType type) {
+    switch (type) {
+      case BirdType.constant:
+        return DashAssets.dashBlueImage;
+      case BirdType.random:
+        return DashAssets.dashGreenImage;
+    }
+  }
+}
+
+class BirdStoreView extends StatelessWidget {
+  final Store store;
+
+  const BirdStoreView({
+    super.key,
+    required this.store,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 8.0,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: StreamBuilder<AppState>(
+            initialData: store.appState,
+            stream: store.getChanges,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return Container();
+              final state = snapshot.requireData;
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: state.birdsItem
+                    .map(
+                      (item) => Opacity(
+                        opacity: item.price <= state.balance ? 1.0 : 0.2,
+                        child: BirdView(
+                          key: ValueKey('$BirdStoreView${item.birdType}'),
+                          birdType: item.birdType,
+                          onTap: item.price <= state.balance ? () => store.buyBird(item) : null,
+                        ),
+                      ),
+                    )
+                    .toList(),
+              );
+            }),
       ),
     );
   }
