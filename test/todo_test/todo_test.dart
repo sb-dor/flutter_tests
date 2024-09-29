511a3610e2b6fe1911075e0cb1bddaf3a9d1da41
sb-dor <avaz.shams.2002@gmail.com>
2024-09-29 22:25:11 +0500

    added first todo test

--- test/todo_test/todo_test.dart
+++ test/todo_test/todo_test.dart
@@ -1,3 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_tests/todo_test/todo_model.dart';
+import 'package:flutter_tests/todo_test/todo_page.dart';
+import 'package:flutter_tests/todo_test/todo_provider.dart';
+import 'package:provider/provider.dart';
 
-void main() {}
+void main() {
+  late TodoProvider provider;
+
+  setUpAll(() {
+    provider = TodoProvider();
+  });
+
+  group(
+    'test todo provider',
+    () {
+      test(
+        'test whether item added inside list',
+        () {
+          const String text = "Have to do homework";
+
+          expect(provider.todos, isEmpty);
+
+          provider.addTodo(text);
+
+          final findTodo = provider.todos.any((todo) => todo.name == text);
+
+          expect(provider.todos.isNotEmpty, true);
+
+          expect(findTodo, true);
+        },
+      );
+    },
+  );
+}
